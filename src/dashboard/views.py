7525fd503242ae18d3116c9ef44d85a24cf3f0d9
Arturo Castillo <arturguay@gmail.com>
2020-09-21 11:11:57 -0500

    Mapped first modal button in business dashboard

--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, redirect
 from restaurants.models import Restaurant
 from django.contrib.auth.decorators import login_required
+from localito.custom_decorators import ajax_required
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.http import JsonResponse
+
 
 
 @login_required
@@ -11,9 +16,17 @@ def dashboardView(request):
         restaurants = Restaurant.objects.filter(owner=request.user)
 
         for restaurant in restaurants:
-            restaurant_dict[restaurant.name] = [restaurant.imageURL, restaurant.get_full_address]
+            restaurant_dict[restaurant.name] = [restaurant.imageURL, restaurant.get_full_address, restaurant.slug]
         
         context['restaurants'] = restaurant_dict
         return render(request, 'dashboard/negocios.html', context)
     else:
         return redirect('home')
+
+@ajax_required
+def getModalURL(request):
+    if request.GET:
+        store_slug = request.GET.get('store')
+        store_url = reverse('restaurant', args=(store_slug,))
+        print(store_url)
+        return JsonResponse({'store_URL': store_url}, status=200)
