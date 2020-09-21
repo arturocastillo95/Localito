b4ed88dc5d99b05b7bf36c6b0e579aa6865b6142
Arturo Castillo <arturguay@gmail.com>
2020-09-20 22:32:43 -0500

    Added non field errors to forms

--- src/pagemanager/views.py
+++ src/pagemanager/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from localito.custom_decorators import anonymous_required
 
 # Create your views here.
 
+@anonymous_required(redirect_url='dashboard')
 def homeScreenView(request):
 	context = {}
 	return render(request, "pagemanager/home.html", context)
