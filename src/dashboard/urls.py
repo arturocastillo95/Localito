8dc4576377ddfc67034fac644a8d9a42740ee7a9
Arturo Castillo <arturguay@gmail.com>
2020-11-26 01:53:46 -0600

    Added first edit products

--- src/dashboard/urls.py
+++ src/dashboard/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import dashboardView, getModalURL, qrDownloadView, restaurantInfoView
+from .views import dashboardView, getModalURL, qrDownloadView, restaurantInfoView, catalogEditView
 
 urlpatterns = [
     path('', dashboardView, name='dashboard'),
     path('<str:restaurant>/qr-code', qrDownloadView, name='qrDownload'),
     path('<str:restaurant>/editar-informacion', restaurantInfoView, name='restaurantInfo'),
+    path('<str:restaurant>/editar-productos', catalogEditView, name='catalogEdit'),
     path('ajax/modalurls', getModalURL, name='getModalURL'),
 ]
--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -29,7 +29,8 @@ def getModalURL(request):
         share_url = request.build_absolute_uri(store_url)
         qr_url = reverse('qrDownload', args=(store_slug,))
         info_url = reverse('restaurantInfo', args=(store_slug,))
-        return JsonResponse({'store_URL': store_url, 'share_URL': share_url, 'qr_URL': qr_url, 'info_URL': info_url}, status=200)
+        edit_url = reverse('catalogEdit', args=(store_slug,))
+        return JsonResponse({'store_URL': store_url, 'share_URL': share_url, 'qr_URL': qr_url, 'info_URL': info_url, 'edit_URL': edit_url }, status=200)
 
 @login_required
 @must_be_owner
@@ -73,3 +74,11 @@ def restaurantInfoView(request, restaurant):
     return render(request, 'dashboard/restaurant_info.html', context)
 
 
+@login_required
+@must_be_owner
+def catalogEditView(request, restaurant):
+    restaurant = Restaurant.objects.get(slug=restaurant)
+    context = {
+        'store_name': restaurant.name,
+        }
+    return render(request, 'dashboard/catalog_edit.html', context)
